d29528464742b55fd9440963b4d64d643157d612
Artsiom Petrouski <bloov@tut.by>
2017-02-26 02:13:16 +0300

    Add new functions to the math utils module

--- Delphi/uMathUtils.pas
+++ Delphi/uMathUtils.pas
@@ -12,17 +12,29 @@ function Max(A, B: Single): Single; overload; inline;
 function Max(A, B: Integer): Integer; overload; inline;
 function Max(A, B: Cardinal): Cardinal; overload; inline;
 
-procedure Swap(var A, B: Double); overload;
-procedure Swap(var A, B: Single); overload;
-procedure Swap(var A, B: Integer); overload;
-procedure Swap(var A, B: Cardinal); overload;
+procedure Swap(var A, B: Double); overload; inline;
+procedure Swap(var A, B: Single); overload; inline;
+procedure Swap(var A, B: Integer); overload; inline;
+procedure Swap(var A, B: Cardinal); overload; inline;
 
 function Clamp(AValue, AMin, AMax: Double): Double; overload;
 function Clamp(AValue, AMin, AMax: Single): Single; overload;
 function Clamp(AValue, AMin, AMax: Integer): Integer; overload;
 function Clamp(AValue, AMin, AMax: Cardinal): Cardinal; overload;
 
-function RandomF(): Single;
+function Sign(AValue: Integer): Integer; overload; inline;
+function Sign(AValue: Single): Integer; overload; inline;
+function Sign(AValue: Double): Integer; overload; inline;
+
+function NearZero(AValue: Integer; Eps: Integer): Boolean; overload; inline;
+function NearZero(AValue: Single; Eps: Single): Boolean; overload; inline;
+function NearZero(AValue: Double; Eps: Double): Boolean; overload; inline;
+
+function NearValue(AValue, ATarget: Integer; Eps: Integer): Boolean; overload; inline;
+function NearValue(AValue, ATarget: Single; Eps: Single): Boolean; overload; inline;
+function NearValue(AValue, ATarget: Double; Eps: Double): Boolean; overload; inline;
+
+function RandomF(): Single; inline;
 
 implementation
 
@@ -68,6 +80,24 @@ function Clamp(AValue, AMin, AMax: Single): Single; {$I uMathUtils.inc}
 function Clamp(AValue, AMin, AMax: Integer): Integer; {$I uMathUtils.inc}
 function Clamp(AValue, AMin, AMax: Cardinal): Cardinal; {$I uMathUtils.inc}
 {$UNDEF Clamp}
+
+{$DEFINE Sign}
+function Sign(AValue: Integer): Integer; {$I uMathUtils.inc}
+function Sign(AValue: Single): Integer; {$I uMathUtils.inc}
+function Sign(AValue: Double): Integer; {$I uMathUtils.inc}
+{$UNDEF Sign}
+
+{$DEFINE NearZero}
+function NearZero(AValue: Integer; Eps: Integer): Boolean; {$I uMathUtils.inc}
+function NearZero(AValue: Single; Eps: Single): Boolean; {$I uMathUtils.inc}
+function NearZero(AValue: Double; Eps: Double): Boolean; {$I uMathUtils.inc}
+{$UNDEF NearZero}
+
+{$DEFINE NearValue}
+function NearValue(AValue, ATarget: Integer; Eps: Integer): Boolean; {$I uMathUtils.inc}
+function NearValue(AValue, ATarget: Single; Eps: Single): Boolean; {$I uMathUtils.inc}
+function NearValue(AValue, ATarget: Double; Eps: Double): Boolean; {$I uMathUtils.inc}
+{$UNDEF NearValue}
 
 function RandomF(): Single;
 begin
